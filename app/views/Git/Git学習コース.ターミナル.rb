918778da90193d8d7c01f5d21d66a0221dde9253
yuki0623 <hil.olas.c222@gmail.com>
2020-09-04 23:25:56 +0900

    Git

---
+++ app/views/Git/Git学習コース.ターミナル.rb
@@ -0,0 +1,26 @@
+mysite $ git initInitialized empty Git repository in /home/progate/first/mysite/.git/
+mysite $
+
+================================================================================
+
+<index.html>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Progate</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>
+    <h1 class="title">Progateへようこそ</h1>
+    <p>Progateはオンラインでプログラミングが学べるサービスです。</p>
+  </body>
+</html>
+
+==============================
+
+<ターミナル>
+git add index.html
+
+================================================================================
